b390cdab805937e9da69dd7273f0b89964cbcea9
AbdullaAlQattan <72339250+AbdullaAlQattan@users.noreply.github.com>
2020-10-22 12:51:07 +0300

    added a new ticket class

---
+++ app02-ticketmachine/Ticket.java
@@ -0,0 +1,25 @@
+
+import java.util.Date;
+/**
+ * Write a description of class Ticket here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Ticket
+{
+    private String destination;
+    private int cost; 
+    private Date date;
+    
+    /**
+     * Constructor for objects of class Ticket
+     */
+    public Ticket()
+    {
+        // initialise instance variables
+        
+    }
+
+    
+}
